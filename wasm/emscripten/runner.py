a96b957b270407aa6304e990c597b33b96cb7eb5
David Hewitt <mail@davidhewitt.dev>
2026-09-06 10:17:14 +0100

    ci: use node from emsdk for emscripten job

--- wasm/emscripten/runner.py
+++ wasm/emscripten/runner.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python
+import os
 import pathlib
 import subprocess
 import sys
 
 p = pathlib.Path(sys.argv[1])
 
-command = ["node", p.name, *sys.argv[2:]]
+command = [os.environ["EMSDK_NODE"], p.name, *sys.argv[2:]]
 print("Running:", " ".join(command))
 
 sys.exit(subprocess.call(command, cwd=p.parent))
